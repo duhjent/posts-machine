0c01ed12afd32a30b2bd5f1acdce8cb1095001f7
Victor Sabov <viktorszabo2002@gmail.com>
2020-04-14 02:06:44 +0300

    created machine jpa repo

---
+++ src/main/java/com/duhjent/postsmachine/data/MachineRepo.java
@@ -0,0 +1,9 @@
+package com.duhjent.postsmachine.data;
+
+import com.duhjent.postsmachine.entities.Machine;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MachineRepo extends CrudRepository<Machine, Long>{
+
+}
--- src/main/java/com/duhjent/postsmachine/entities/Machine.java
+++ src/main/java/com/duhjent/postsmachine/entities/Machine.java
@@ -3,17 +3,28 @@ package com.duhjent.postsmachine.entities;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import com.duhjent.postsmachine.MachineException;
 import com.duhjent.postsmachine.entities.commands.Command;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import lombok.Data;
 
 @Entity
 @Data
 public class Machine {
+    @Id
+    private Long id;
     private List<Command> commands;
     private Tape tape;
+
+    @Autowired
+    public Machine(List<Command> commands, Tape tape){
+        this.commands = commands;
+        this.tape = tape;
+    }
 
     public void execute() throws MachineException{
         //TODO
