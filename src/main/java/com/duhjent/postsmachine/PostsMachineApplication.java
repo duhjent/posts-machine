36e4d64241c034941223888c61470c1ea13f15b6
Victor Sabov <viktorszabo2002@gmail.com>
2020-04-13 16:38:43 +0300

    init repo

---
+++ src/main/java/com/duhjent/postsmachine/PostsMachineApplication.java
@@ -0,0 +1,13 @@
+package com.duhjent.postsmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duhjent/postsmachine/PostsMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duhjent.postsmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
