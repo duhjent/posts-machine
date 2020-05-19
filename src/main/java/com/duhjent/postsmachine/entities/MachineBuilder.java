d5f736b2fe46a414d284a9b20105176a22292597
Victor Sabov <viktorszabo2002@gmail.com>
2020-05-19 20:15:34 +0300

    added regexp to validate tape

--- src/main/java/com/duhjent/postsmachine/entities/MachineBuilder.java
+++ src/main/java/com/duhjent/postsmachine/entities/MachineBuilder.java
@@ -1,6 +1,7 @@
 package com.duhjent.postsmachine.entities;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import com.duhjent.postsmachine.entities.commands.CommandReader;
@@ -13,6 +14,7 @@ import lombok.Data;
 @Data
 public class MachineBuilder {
     @NotBlank(message = "The tape shouldn't be empty")
+    @Pattern(regexp = "^[0-1]*$", message = "The tape should only contain 0s and 1s")
     private String tapeString;
 
     @Size(min = 5, message = "The name must be at least 5 characters long")
